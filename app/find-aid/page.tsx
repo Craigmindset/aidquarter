8f4b55d02f11bc63b0405447bc0087e05d72ec06
craigmindset <irediacraig@gmail.com>
2026-03-01 00:29:03 +0100

    added mt10

--- app/find-aid/page.tsx
+++ app/find-aid/page.tsx
@@ -167,7 +167,7 @@ export default function FindAidPage() {
     <div className="min-h-screen bg-gray-50">
       {/* Banner */}
       <section className="bg-gradient-to-r from-green-600 to-blue-600 text-white py-16">
-        <div className="container mx-auto px-4 lg:px-6 text-center">
+        <div className="container mx-auto px-4 lg:px-6 text-center mt-10">
           <h1 className="text-4xl lg:text-5xl font-bold mb-4">
             Find Your Perfect Household Staff
           </h1>
